3776a71300daa899d5a4226430b45763efcc4a0c
Bozzy777 <baydilda.zhalgas@bk.ru>
2026-02-07 00:31:31 +0500

    firebase is ready

---
+++ src/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQDwVv2h6n2bNSb0ngejuag3oC_5q3Xs4",
+  authDomain: "templatex-a5385.firebaseapp.com",
+  projectId: "templatex-a5385",
+  storageBucket: "templatex-a5385.firebasestorage.app",
+  messagingSenderId: "233204350462",
+  appId: "1:233204350462:web:ec598ec9bcf16e3f4bd4af",
+  measurementId: "G-FCL4FWY4GN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export { app , analytics}
